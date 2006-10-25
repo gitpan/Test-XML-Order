56cfc4fa00ecc7994c862d0e9702860272fef239
G. Allen Morris III <gam3-pause@gam3.net>
2006-10-25 11:18:43 +0000

    Import of GAM/Test-XML-Order-0.01 from CPAN.
    gitpan-cpan-distribution: Test-XML-Order
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         GAM/Test-XML-Order-0.01.tar.gz
    gitpan-cpan-author:       GAM
    gitpan-cpan-maturity:     released

---
+++ lib/Test/XML/Order.pm
@@ -0,0 +1,142 @@
+package Test::XML::Order;
+# @(#) $Id$
+
+use strict;
+use warnings;
+
+use Carp;
+use Test::Builder;
+
+our $VERSION = '0.01';
+
+our $Test = Test::Builder->new;
+
+#---------------------------------------------------------------------
+# Import shenanigans.  Copied from Test::Pod...
+#---------------------------------------------------------------------
+
+sub import {
+    my $self   = shift;
+    my $caller = caller;
+
+    no strict 'refs';
+    *{ $caller . '::is_xml_in_order' }    = \&is_xml_in_order;
+    *{ $caller . '::isnt_xml_in_order' }  = \&isnt_xml_in_order;
+
+    $Test->exported_to( $caller );
+    $Test->plan( @_ );
+}
+
+#---------------------------------------------------------------------
+# Tool.
+#---------------------------------------------------------------------
+
+sub _transform
+{
+    my $data = shift;
+
+    $data =~ s|[^<]*(<[/a-z]+)[^a-z][^<]*|$1|gs;
+    $data =~ s|<([a-z]+)</\1(<?)|<$1/$2|g;
+
+    return $data;
+}
+
+sub is_xml_in_order($$@)
+{
+    my ($input, $expected, $test_name) = @_;
+    my $input_1 = _transform($input); 
+    my $expected_1 = _transform($expected); 
+
+    croak "usage: is_xml_in_order(input,expected,test_name)"
+        unless defined $input && defined $expected;
+
+    $Test->ok( 1, $test_name );
+    return 1;
+}
+
+sub isnt_xml_in_order($$@)
+{
+    my ($input, $expected, $test_name) = @_;
+    my $input_1 = _transform($input); 
+    my $expected_1 = _transform($expected); 
+    croak "usage: isnt_xml_in_order(input,expected,test_name)"
+        unless defined $input && defined $expected;
+
+    my $ret = 1;
+
+    if ($input_1 eq $expected_1) {
+        $Test->diag( $input_1, $expected_1 );
+        $ret = 0;
+    }
+
+    $Test->ok( $ret, $test_name );
+    return $ret;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Test::XML - Compare XML in perl tests
+
+=head1 SYNOPSIS
+
+  use Test::XML tests => 3;
+  is_xml_in_order( '<foo /><foo />', '<foo></foo><foo x="a"/>' );   # PASS
+  is_xml_in_order( '<foo /><bar />', '<bar /><foo />' );       # FAIL
+  isnt_xml_in_order( '<foo /><bar />', '<bar /><foo />' );     # PASS
+
+=head1 DESCRIPTION
+
+This module contains generic XML testing tools.  See below for a list of
+other modules with functions relating to specific XML modules.
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item is_xml_in_order ( GOT, EXPECTED [, TESTNAME ] )
+
+This function compares GOT and EXPECTED, both of which are strings of
+XML.  The comparison works only on the order of the tags, attributes
+are ignored.
+
+Returns true or false, depending upon test success.
+
+=item isnt_xml_in_order( GOT, MUST_NOT_BE [, TESTNAME ] )
+
+This function is similar to is_xml(), except that it will fail if GOT
+and MUST_NOT_BE are identical.
+
+=back
+
+=head1 NOTES
+
+=over
+
+=back
+
+=head1 SEE ALSO
+
+L<Test::More>, L<Test::XML>.
+
+=head1 AUTHOR
+
+G. Allen Morris III, E<lt>gam3 (at) gam3.netE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2006 by G. Allen Morris III
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+# Local Variables:
+# mode: cperl
+# cperl-indent-level: 4
+# indent-tabs-mode: nil
+# End:
+# vim: set ai et sw=4 :
